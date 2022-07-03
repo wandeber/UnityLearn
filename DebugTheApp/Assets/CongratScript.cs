f42f8c151d5c898d0d3855c2c57492f4c0519201
wandeber <berthewanderer@gmail.com>
2022-07-04 00:10:58 +0200

    Debug the app finished.

---
+++ DebugTheApp/Assets/CongratScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratScript : MonoBehaviour
+{
+    public TextMesh Text;
+    public ParticleSystem SparksParticles;
+    
+    private List<string> TextToDisplay = new List<string>();
+    
+    private float RotatingSpeed;
+    private float TimeToNextText;
+
+    private int CurrentText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeToNextText = 0.0f;
+        CurrentText = 0;
+        
+        RotatingSpeed = 1.0f;
+
+        TextToDisplay.Add("Congratulation");
+        TextToDisplay.Add("All Errors Fixed");
+
+        Text.text = TextToDisplay[0];
+        
+        SparksParticles.Play();
+        // Rotate the text
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeToNextText += Time.deltaTime;
+        // Rotate particle system
+        gameObject.transform.Rotate(0.0f, RotatingSpeed * Time.deltaTime * 20f, 0.0f);
+        
+
+        if (TimeToNextText > 1.5f)
+        {
+            TimeToNextText = 0.0f;
+            
+            CurrentText++;
+            if (CurrentText >= TextToDisplay.Count)
+            {
+                CurrentText = 0;
+            }
+
+
+            Text.text = TextToDisplay[CurrentText];
+        }
+    }
+}
