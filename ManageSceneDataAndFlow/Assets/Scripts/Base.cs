c3f7d02a9ff9897eeb039a1176f45673d1c10985
wandeber <berthewanderer@gmail.com>
2022-07-08 20:56:11 +0200

    Data persistence.

--- ManageSceneDataAndFlow/Assets/Scripts/Base.cs
+++ ManageSceneDataAndFlow/Assets/Scripts/Base.cs
@@ -7,12 +7,10 @@ using UnityEngine;
 /// A special building that hold a static reference so it can be found by other script easily (e.g. for Unit to go back
 /// to it)
 /// </summary>
-public class Base : Building
-{ 
-    public static Base Instance { get; private set; }
+public class Base: Building {
+  public static Base Instance { get; private set; }
 
-    private void Awake()
-    {
-        Instance = this;
-    }
+  private void Awake() {
+    Instance = this;
+  }
 }
--- ManageSceneDataAndFlow/Assets/Scripts/MenuUIHandler.cs
+++ ManageSceneDataAndFlow/Assets/Scripts/MenuUIHandler.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 #if UNITY_EDITOR
-  using UnityEditor;
+using UnityEditor;
 #endif
 
 
@@ -17,12 +17,14 @@ public class MenuUIHandler: MonoBehaviour {
 
   public void NewColorSelected(Color color) {
     // add code here to handle when a color is selected
+    MainManager.Instance.TeamColor = color;
   }
-  
+
   private void Start() {
     ColorPicker.Init();
     //this will call the NewColorSelected function when the color picker have a color button clicked.
     ColorPicker.onColorChanged += NewColorSelected;
+    ColorPicker.SelectColor(MainManager.Instance.TeamColor);
   }
 
   public void StartNew() {
@@ -30,11 +32,21 @@ public class MenuUIHandler: MonoBehaviour {
   }
 
   public void Exit() {
-    #if UNITY_EDITOR
-      //UnityEditor.EditorApplication.isPlaying = false;
-      EditorApplication.ExitPlaymode();
-    #else
+    MainManager.Instance.SaveColor();
+#if UNITY_EDITOR
+    //UnityEditor.EditorApplication.isPlaying = false;
+    EditorApplication.ExitPlaymode();
+#else
       Application.Quit();
-    #endif
+#endif
+  }
+
+  public void SaveColorClicked() {
+    MainManager.Instance.SaveColor();
+  }
+
+  public void LoadColorClicked() {
+    MainManager.Instance.LoadColor();
+    ColorPicker.SelectColor(MainManager.Instance.TeamColor);
   }
 }
